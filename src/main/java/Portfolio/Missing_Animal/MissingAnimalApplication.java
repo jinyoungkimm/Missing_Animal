90399812dcb09383f8d0da7009cab9a229302cb2
Jin Young Kim <wlsdud66523@kakao.com>
2023-07-04 01:42:34 +0900

    Setted HTTPS

--- src/main/java/Portfolio/Missing_Animal/MissingAnimalApplication.java
+++ src/main/java/Portfolio/Missing_Animal/MissingAnimalApplication.java
@@ -2,16 +2,25 @@ package Portfolio.Missing_Animal;
 
 import Portfolio.Missing_Animal.service.serviceinterface.StorageService;
 import Portfolio.Missing_Animal.propertiesWithJava.StorageProperties;
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
 public class MissingAnimalApplication {
+
 
 	public static void main(String[] args) {
 
@@ -33,6 +42,32 @@ public class MissingAnimalApplication {
 
 			initMissingAddress.init();
 		};
+	}
+
+
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {@Override
+		protected void postProcessContext(Context context) {
+			SecurityConstraint securityConstraint = new SecurityConstraint();
+			securityConstraint.setUserConstraint("CONFIDENTIAL");
+			SecurityCollection collection = new SecurityCollection();
+			collection.addPattern("/*");
+			securityConstraint.addCollection(collection);
+			context.addConstraint(securityConstraint);
+		}
+		};
+		tomcat.addAdditionalTomcatConnectors(redirectConnector());
+		return tomcat;
+	}
+
+	private Connector redirectConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setScheme("http");
+		connector.setPort(8080);
+		connector.setSecure(false);
+		connector.setRedirectPort(8443);
+		return connector;
 	}
 
 }
