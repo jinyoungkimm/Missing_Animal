90ad63b687c84d48b4ab0b5b9ba2df74b8c7992d
Jin Young Kim <wlsdud66523@kakao.com>
2023-08-03 21:48:56 +0900

    채팅 기능서버와 연동

--- src/main/java/Portfolio/Missing_Animal/controller/HomeController.java
+++ src/main/java/Portfolio/Missing_Animal/controller/HomeController.java
@@ -1,14 +1,25 @@
 package Portfolio.Missing_Animal.controller;
 
 
+import lombok.Data;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
 
 @Controller
 public class HomeController {
 
+    public static HashMap<String,String> chatRoomNum = new HashMap<>();
+
+
+
     @RequestMapping("/")
     public String home(){
+
 
         return "home";
 
@@ -33,6 +44,40 @@ public class HomeController {
 
     }
 
+    @RequestMapping("/chatInit")
+    String chat(){
+
+        return "chatInit";
+
+    }
+
+    @GetMapping("/chatPage")
+    String chatPage(//@RequestParam("sender") String sender,
+            @RequestParam("receiver") String receiver,
+            Model model){
+
+        String sender = "wlsdud6523";
+        Chat chat = new Chat();
+        chat.setSender(sender);
+        chat.setReceiver(receiver);
+        chat.setRoomNum(sender+receiver);
+
+        model.addAttribute("chat",chat);
+
+
+        return "chat";
+    }
+
+
+    @Data
+    static class Chat{
+
+        private String sender;
+        private String receiver;
+        private String roomNum;
+
+
+    }
 
 
 }
